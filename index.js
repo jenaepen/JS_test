f3f2d1b9af2cf6d4d7ee16ac95be974d51426005
jenae <jenaepen@gmail.com>
2020-05-01 17:53:34 -0400

    updated getWeatherAndTimezone parameters

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
- * getTimeAndWeather takes in an array of location and its zipcode
+ * getTimeAndWeather takes in an array of location (as a String)
  * console logs weather and time 
  * It iterates over the array while doing the following:
- *  1) invokes getWeatherAndTimezone with a location and zipcode
+ *  1) invokes getWeatherAndTimezone with a location 
  *  2) declare and initialize weather and timezone from getWeatherAndTimezone
  *  3) invokes getTime with timezone
  *  4) declare and initialize time from timezone
@@ -10,22 +10,22 @@
  */
 
  function getTimeAndWeather(array){
-    for(let [location,zipcode] of array){
-       const [weather, timezone] = getWeatherAndTimezone(location, zipcode)
+    for(let location of array){
+       const [weather, timezone] = getWeatherAndTimezone(location)
        const time = getTime(timezone)
        console.log("weather: " + weather + "time: " +time)
     }
  }
 
  /**
- * getWeatherAndTimezone takes in an location and its zipcode
+ * getWeatherAndTimezone takes in an location
  * returns the weather (as a String) and timezone (as a number) within an array
- * It declares and initialize url with the location and zipcode
+ * It declares and initialize url with the location 
  * Fetches url by using open weather's api 
  * Then destructing the weather.description and timezone from the returned data object
  * returning weather and timezone within an array
  */
- function getWeatherAndTimezone(location, zipcode){
+ function getWeatherAndTimezone(location){
 
  }
 
