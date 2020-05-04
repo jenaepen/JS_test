51f96127dc6e9711df122c7f08b4ec1f156c4639
jenae <jenaepen@gmail.com>
2020-05-03 22:10:53 -0400

    added new outputs for getTimeAndWeather function

--- getTimeAndWeather.test.js
+++ getTimeAndWeather.test.js
@@ -7,24 +7,32 @@ describe('Test getTimeAndWeather function', () => {
         jest.clearAllMocks();
       });
 
-    it('accept as array"', async () => {
-        const consoleSpy = jest.spyOn(console, "log"); 
+    it('accept as array"',  async () => {
+        const consoleSpy = jest.spyOn(console, "log");
         await getTimeAndWeather(["New York"])
-        expect(consoleSpy).toHaveBeenCalledWith('weather: nice weather time: 12:00:00');   
+        expect(consoleSpy).toHaveBeenCalledWith('Input: New York\nOutput: weather: nice weather time: 12:00:00\n');
     });
-    it('accept array with multiple input"', async () => {
+    it('accept array with multiple input', async () => {
         const consoleSpy = jest.spyOn(console, "log"); 
-        await getTimeAndWeather(["New York","New York, 10278","London","fake",""])
-        expect(consoleSpy).toHaveBeenCalledWith('weather: nice weather time: 12:00:00');
-        expect(consoleSpy).toHaveBeenCalledWith('weather: bad weather time: 12:00:00');
-        expect(consoleSpy).toHaveBeenCalledWith('weather: clear skies time: 17:00:00');
+        await getTimeAndWeather(["New York","New York, 10278","London"]);
+        expect(consoleSpy).toHaveBeenCalledWith('Input: New York\nOutput: weather: nice weather time: 12:00:00\n');
+        expect(consoleSpy).toHaveBeenCalledWith('Input: New York, 10278\nOutput: weather: bad weather time: 12:00:00\n');
+        expect(consoleSpy).toHaveBeenCalledWith('Input: London\nOutput: weather: clear skies time: 17:00:00\n');
     });
-    it('does not console log if input is not an array"', async () => {
+    it('A error is provided for invalid inputs', async () => {
+        try{
+          getTimeAndWeather(["","fake"]);  
+        } 
+        catch(e){
+            expect(e).toEqual('Input: \nOutput: weather: error time: error\n')
+        }
+    });
+    it('does not console log if input is not an array', async () => {
         const consoleSpy = jest.spyOn(console, "log"); 
         await getTimeAndWeather("New York")
         expect(consoleSpy).not.toHaveBeenCalled();
     });
-    it('expect no calls with an empty array as an input"', async () => {
+    it('expect no calls with an empty array as an input', async () => {
         const consoleSpy = jest.spyOn(console, "log"); 
         await getTimeAndWeather([])
         expect(consoleSpy).not.toHaveBeenCalled();
